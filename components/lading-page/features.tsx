66e9dae0d9b846e6095dc56def0cae54c9781896
Rahul Siddhardh <siddhardh6002@gmail.com>
2024-10-22 20:35:22 +0530

    added testimonials and pricing section

--- components/lading-page/features.tsx
+++ components/lading-page/features.tsx
@@ -37,21 +37,21 @@ const featuresInfo = [
 
 function Features() {
   return (
-    <div className='w-full flex flex-col my-36 space-y-16'>
+    <div className='w-full my-36 space-y-16'>
         <div className='text-center space-y-6'>
-            <h2 className='text-xl font-semibold text-gray-700'>Features</h2>
+            <h3 className='text-md lg:text-lg xl:text-xl font-semibold text-gray-700'>Features</h3>
             <div className='flex flex-col items-center space-y-9'>
-                <h3 className='text-4xl font-bold text-gray-900'>The premier choice for developers and businesses alike</h3>
-                <p className='text-lg max-w-3xl text-gray-600'>With robust security features, real-time backup, and user-friendly interfaces, gives you the peace of mind that comes with reliable and efficient data management</p>
+                <h2 className='text-2xl lg:text-3xl xl:text-4xl font-bold text-gray-900'>The premier choice for developers and businesses alike</h2>
+                <p className='text-sm lg:text-md xl:text-lg max-w-3xl text-gray-600'>With robust security features, real-time backup, and user-friendly interfaces, gives you the peace of mind that comes with reliable and efficient data management</p>
             </div>
         </div>
-        <div className='w-full grid grid-cols-3 gap-6'>
+        <div className='w-full grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
             {featuresInfo.map((feature, index) => (
-                <div key={index} className='span-col-1 flex flex-col items-center gap-y-6'>
-                    <feature.icon className='w-9 h-9 fill-blue-600 hover:fill-zinc-800'/>
+                <div key={index} className='span-col-1 flex flex-col items-center gap-y-2 sm:gap-y-6 sm:bg-gray-50 p-6 sm:p-8 sm:rounded-2xl sm:shadow-md sm:shadow-neutral-400/40 group'>
+                    <feature.icon className='w-7 h-7 lg:w-8 lg:h-8 xl:w-9 xl:h-9 fill-blue-600 hover:fill-zinc-800 lg:group-hover:animate-tilt lg:transition-transform lg:duration-500'/>
                     <div className='text-center space-y-6'>
-                        <h4 className='text-lg font-semibold text-gray-700'>{feature.title}</h4>
-                        <p className='text-md max-w-xs text-gray-500'>{feature.description}</p>
+                        <h4 className='text-sm lg:text-md xl:text-lg font-semibold text-zinc-700'>{feature.title}</h4>
+                        <p className='text-xs lg:text-sm xl:text-md max-w-xs text-zinc-500'>{feature.description}</p>
                     </div>
                 </div>
             ))}
